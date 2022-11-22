57e8a502a428c7489d8db9596c6dbecaf9de2f7b
AlessandroUbe <115072327+AlessandroUbe@users.noreply.github.com>
2022-11-22 16:34:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+   
+   
+   int n=0;
+    int f= 1, f1=1;
+    cin >> n;
+    if(n>=2) {
+        cout << f << endl;
+        cout << f1 << endl;
+        for (int i = 1; i < n - 1; i++) {
+            int temp = f;
+            f += f1;
+            f1 = temp;
+            cout << f << endl;
+        }
+    }else{
+        cout << "errore"<<endl;
+    }
    return 0;
 }
